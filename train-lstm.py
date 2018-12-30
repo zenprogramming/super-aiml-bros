546a83b4e17c4eb57c0a05105a9710f97c1e122d
Chuck Marshall <cmarshal@zenprogramming.com>
2018-12-29 22:14:48 -0500

    Work In Progress

---
+++ train-lstm.py
@@ -0,0 +1,7 @@
+def build_dataset(words):
+    count = collections.Counter(words).most_common()
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return dictionary, reverse_dictionary
